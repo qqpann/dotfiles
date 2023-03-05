1bdfae6506114de66d9322803e3a653ea94fda95
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2023-03-06 02:44:45 +0900

    chore: link vimrc

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
 # echo "Install brew in advance."
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/vim/vimrc ~/.vim_runtime/my_configs.vim
 
 git -C "$HOME/.dotfiles" update-index --assume-unchanged ~/.dotfiles/zsh/.zshrc
 
